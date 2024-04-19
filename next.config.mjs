8205fd8b71a405399f2d72117515e62724e3861d
Yannick Tirand <ytirand@gmail.com>
2024-04-19 12:43:56 +1200

    Updates next.config.js to use ENV variables for the remotePatterns images options

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
+import env from "@next/env";
+env.loadEnvConfig(".");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "localhost",
-        port: "",
+        protocol: process.env.WORDPRESS_API_PROTOCOL,
+        hostname: process.env.WORDPRESS_API_HOST,
+        port: process.env.WORDPRESS_API_PORT,
         pathname: "/**",
       },
     ],
@@ -15,5 +18,7 @@ const nextConfig = {
     prependData: '@import "@/styles/_variables.scss";\n@import "@/styles/_mixins.scss";\n@import "@/styles/main.scss";\n'
   }
 };
+
+console.log(nextConfig.images.remotePatterns);
 
 export default nextConfig;
