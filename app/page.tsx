f708b2f001ecfa5be9e896ebc47d7b17f0e2e94e
Yannick Tirand <ytirand@gmail.com>
2024-04-30 13:39:06 +1200

    Updates the page.tsx to use the new Sections data coming from Wordpress to build the page; Updates nav styling in accordance

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import styles from "@/app/page.module.scss";
 
-import { AlertPositions, AlertProvider, FontsProvider } from "ydl-react-components";
+import { AlertPositions, AlertProvider, FontsProvider, WordpressAPI } from "ydl-react-components";
 import NavbarWrapper from "@/app/ui/content/navbar/navbar-wrapper";
 import Highlights from "@/app/ui/content/highlights/highlights";
 import Galleries from "@/app/ui/content/gallery/galleries";
@@ -8,18 +8,30 @@ import Footer from "@/app/ui/content/footer/footer";
 import AboutWrapper from "@/app/ui/content/about/about-wrapper";
 import ContactWrapper from "@/app/ui/content/contact/contact-wrapper";
 import { primaryFont, secondaryFont } from "@/app/styles/fonts";
+import { ReactNode } from "react";
 
-export default function Home() {
+/**
+ * Lookup table between section name and corresponding Component.
+ */
+const sectionsLookupTable: {[key: string]: ReactNode} = {
+  "highlights": <Highlights />,
+  "galleries": <Galleries />,
+  "about": <AboutWrapper />,
+  "contact": <ContactWrapper /> 
+};
+
+export default async function Home() {
+  const sections = await WordpressAPI.fetchSections();
+  
   return (
     <FontsProvider primaryFont={primaryFont} secondaryFont={secondaryFont}>
       <AlertProvider position={AlertPositions.BOTTOM_LEFT} offset="100px">
         <main>
           <div id="main-container" className={styles.snapScrollContainer}>
             <NavbarWrapper />
-            <Highlights />
-            <Galleries />
-            <AboutWrapper />
-            <ContactWrapper />
+            {sections.map(section => {
+              return sectionsLookupTable[section];
+            })}
             <Footer />
           </div>
         </main>
